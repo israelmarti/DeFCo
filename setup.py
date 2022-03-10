9959818cf6d999cb4f4f97a737269b0953612632
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2022-03-10 18:03:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'defco',
   packages = ['defco'], # this must be the same as the name above
   version = '0.1',
-  description = 'Python code to detect secondary companion in a binary spectroscopy',
+  description = 'Python script to detect secondary companion in a binary spectroscopy',
   author = 'Cristian I. Martinez',
   author_email = 'cmartinez@conicet.gov.ar',
   url = 'https://github.com/israelmarti/DeFCo', # use the URL to the github repo
