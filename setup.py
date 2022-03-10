7c5fbeb01fbc32b4bcc1d89b5eae4d4ef9910656
Cristian Israel Martínez <101339211+israelmarti@users.noreply.github.com>
2022-03-10 14:37:16 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'defco',
+  packages = ['defco'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python code to detect secondary companion in a binary spectroscopy',
+  author = 'Cristian I. Martinez',
+  author_email = 'cmartinez@conicet.gov.ar',
+  url = 'https://github.com/israelmarti/DeFCo', # use the URL to the github repo
+  download_url = 'https://github.com/israelmarti/DeFCo/tarball/0.1', # I'll explain this in a second
+  keywords = ['AutomaticApiRest', 'Connector'], # arbitrary keywords
+  classifiers = [],
+)
